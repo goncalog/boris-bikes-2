ac6f37828078cbe2900f1fd2a67adb8ed8a273f4
Leslie <leslie.liszcz@gmail.com>
2021-06-02 16:10:20 +0100

    full-test implementation cycle for 2nd user

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,8 +1,18 @@
 require 'bike'
 
 class DockingStation 
+  attr_reader :bike_dock
+
+  def initialize
+    @bike_dock = []
+  end
+
   def release_bike
     new_bike = Bike.new
+  end
+
+  def dock_bike
+    @bike_dock << Bike.new
   end
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -9,4 +9,14 @@ describe DockingStation do
     expect(subject.release_bike.working?).to eq true
   end
 
+  it 'responds to dock_bike' do
+    expect(subject).to respond_to(:dock_bike)
+  end
+
+  it 'expects bikes to be docked' do
+    dockingstation = DockingStation.new
+    dockingstation.dock_bike
+    expect(dockingstation.bike_dock.empty?).to eq false
+  end
+
 end
